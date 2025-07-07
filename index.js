808d2356c12e76ef9780b7084ce9f8ad48c7de31
= <=>
2025-07-07 11:55:27 +0500

    crud completed

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 app.use(express.json())
 
 const userModel = require('./usermodel')
+const mongoose = require("mongoose")
 
 app.post('/addstudent', async (req,res)=>{
    try {
@@ -50,45 +51,270 @@ app.get("/get-data",async (req,res)=>{
     }
 })
 
-
-app.get("/getuser/:id",async (req,res)=>{
-    const {id} =  req.params
-        if(!mongoose.Types.ObjectId.isValid(id)){
-            res.status(400).json({
-                "statuscode":400,
+app.get("/getuserdetails",async(req,res)=>{
+    const {id} = req.params
+    
+    if(!mongoose.isValidObjectId(id)){
+        return  res.status(400).json({
+            "statuscode":400,
                 "success":false,
-                "message": "invaild object type",
+                "message": "Bad Requeest",
+                "data" : {
+                    "message" : "please write vaild id"
+                }
+        })
+    }
+    try {
+        const userdata = await userModel.findById(id)
+
+        if(!userdata){
+            return res.status(404).json({
+                "statuscode":404,
+                "success":false,
+                "message": "User Not found",
                 "data" : null
+            })
+        }
+        return res.json({
+            "status" : 200,
+            "success" : true,
+            "message" : "data has been shown",
+            "data" :userdata
+        })
+    } catch (error) {
+        return res.status(500).json({
+                        "statuscode":500,
+                        "success":false,
+                        "message": "Internal Server Error",
+                        "data" : {
+                            "error" :error.message
+                        }
+                    })
+    }
+})
+app.delete("/delete",async (req,res)=>{
+    const {id} =  req.params
+           if(!mongoose.isValidObjectId(id)){
+           return res.status(404).json({
+                "statuscode":404,
+                "success":false,
+                "message": "Bad Requeest",
+                "data" : {
+                    "message" : "please write vaild id"
+                }
             })
         }
     try {
-        let userdata = await usermodel.findById(id)
-        if(!userdata){
-            res.status(404).json({
+        const userdelete= await findByIdAndDelete(id)
+        if(!userdelete){
+            return res.status(404).json({
                 "statuscode":404,
                 "success":false,
-                "message": "user not found",
+                "message": "User Not found",
                 "data" : null
             })
         }
-        res.json({
-            "statuscode":200,
-            "success":true,
-            "message": "data is feteched",
-            "data" : userdata
+        return res.json({
+            "status" : 200,
+            "success" : true,
+            "message" : "User deleted",
         })
     } catch (error) {
-        res.status(500).json({
+        return res.status(500).json({
             "statuscode":500,
             "success":false,
-            "message": error.message,
-            "data" : null
+            "message": "Internal Server Error",
+            "data" : {
+                "error" :error.message
+            }
         })
     }
-
 })
+app.put("/update",async(req,res)=>{
+    const { id,names , emails ,batchs ,ages,   is_course_completeds } = req.body
+    if(!mongoose.isValidObjectId(id)){
+        return res.status(404).json({
+             "statuscode":404,
+             "success":false,
+             "message": "Bad Requeest",
+             "data" : {
+                 "message" : "please write vaild id"
+             }
+         })
+     }
+    try {
+        const userupdate =  await userModel.findByIdAndUpdate(id,{
+            name :names,
+            email :emails,
+            batch : batchs,
+            age :ages,
+            is_course_completed : is_course_completeds
+        },
+        {new:true}
+        )
+        return res.json({
+            "status" : 200,
+            "success" : true,
+            "message" : "re",
+            "data" :userupdate
+        })
+    } catch (error) {
+        return res.status(500).json({
+            "statuscode":500,
+            "success":false,
+            "message": "Internal Server Error",
+            "data" : {
+                "error" :error.message
+            }
+        })
+    }
+})
+// app.get("/getuserdetails", async (req,res)=>{
+//     const {id} = req.query
+//         if(!mongoose.isValidObjectId(id)){
+//             res.status(404).json({
+//                 "statuscode":404,
+//                 "success":false,
+//                 "message": "Bad Requeest",
+//                 "data" : {
+//                     "message" : "please write vaild id"
+//                 }
+//             })
+//         }
+//     try {
+//        const  userdata = await  userModel.findById(id)
+//        if(!userdata){
+//         res.status(404).json({
+//             "statuscode":404,
+//             "success":false,
+//             "message": "invaild user",
+//             "data" : null
+//         })
+//        }
+//        res.json({
+//                     "statuscode":200,
+//                     "success":true,
+//                     "message": "data is feteched",
+//                     "data" : userdata
+//                 })
+//     } catch (error) {
+//         res.status(500).json({
+//             "statuscode":500,
+//             "success":false,
+//             "message": "Internal Server Error",
+//             "data" : {
+//                 "error" :error.message
+//             }
+//         })
+//     }
+//  })
 
-app.listen(4000)
+//  app.delete("/delete/:id",async(req,res)=>{
+//     const {id} = req.params
+//     try {
+//             const userdata = await userModel.findByIdAndDelete(id)
+//             res.json({
+//                 "statuscode":200,
+//                 "success":true,
+//                 "message": "User is deleted",
+                
+//             })
+//     } catch (error) {
+//         res.status(500).json({
+//             "statuscode":500,
+//             "success":false,
+//             "message": "Internal Server Error",
+//             "data" : {
+//                 "error" :error.message
+//             }
+//         })
+//     }
+//  })
+
+//  app.put("/updateusers",async (req,res)=>{
+//     const {id, names , emails ,batchs ,ages, is_course_completeds } = req.body
+//             if(!mongoose.Types.ObjectId.isValid(id)){
+//            return res.status(400).json({
+//                 "statuscode":400,
+//                 "success":false,
+//                 "message": "invaild object type",
+//                 "data" : null
+//             })
+//         }
+//    try {
+//     const userdata = await userModel.findByIdAndUpdate(id,{
+//         name : names,
+//         email :emails,
+//         batch  : batchs,
+//         age : ages,
+//         is_course_completed : is_course_completeds
+//     },
+//     { new: true }
+// )
+//         if(!userdata){
+//             return res.status(404).json({
+//                 "statuscode":404,
+//                 "success":false,
+//                 "message": "invaild user",
+//                 "data" : null
+//             })
+//         }
+
+//          res.json({
+//             "statuscode":200,
+//             "success":true,
+//             "message": "data is feteched",
+//             "data" : userdata
+//         })
+//    } catch (error) {
+//    return res.status(500).json({
+//         "statuscode":500,
+//         "success":false,
+//         "message": "Internal Server Error",
+//         "data" : {
+//             "error" :error.message
+//         }
+//     })
+//    }
+//  })
+// app.get("/getuser/:id",async (req,res)=>{
+//     const {id} =  req.params
+//         if(!mongoose.Types.ObjectId.isValid(id)){
+//             res.status(400).json({
+//                 "statuscode":400,
+//                 "success":false,
+//                 "message": "invaild object type",
+//                 "data" : null
+//             })
+//         }
+//     try {
+//         let userdata = await usermodel.findById(id)
+//         if(!userdata){
+//             res.status(404).json({
+//                 "statuscode":404,
+//                 "success":false,
+//                 "message": "user not found",
+//                 "data" : null
+//             })
+//         }
+//         res.json({
+//             "statuscode":200,
+//             "success":true,
+//             "message": "data is feteched",
+//             "data" : userdata
+//         })
+//     } catch (error) {
+//         res.status(500).json({
+//             "statuscode":500,
+//             "success":false,
+//             "message": error.message,
+//             "data" : null
+//         })
+//     }
+
+// })
+
+ app.listen(4000)
 
 
 
